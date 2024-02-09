1b65c2dcfff1170de596d2c31de338dd3beede9f
DAGMAWI-AT <vvman1717@gmail.com>
2024-02-09 05:39:32 -0800

    server index.js

--- Server/index.js
+++ Server/index.js
@@ -66,18 +66,27 @@ const  bannercollaction= client.db("bazra").collection("banner")
   //     const result=await bannercollaction.insertOne(data);
   //     res.send(result);
   // })
-      app.post("/addbanner", upload.single('imageFile'), async (req, res) => {
-  try {
-    const data = req.body;
-    // Assuming 'bannercollaction' is a collection instance
-    const result = await bannercollaction.insertOne(data);
-
-    res.json({ success: true, message: 'Banner added successfully', result });
-  } catch (error) {
-    console.error('Error adding banner:', error);
-    res.status(500).json({ success: false, message: 'Internal Server Error' });
-  }
-});
+  app.post("/addbanner", upload.single('imageFile'), async (req, res) => {
+    try {
+      const data = req.body;
+      const imageFile = req.file;
+  
+      // Assuming 'bannercollaction' is a collection instance
+      const result = await bannercollaction.insertOne({
+        ...data,
+        imageFile: {
+          data: imageFile.buffer,
+          contentType: imageFile.mimetype
+        }
+      });
+  
+      res.json({ success: true, message: 'Banner added successfully', result });
+    } catch (error) {
+      console.error('Error adding banner:', error);
+      res.status(500).json({ success: false, message: 'Internal Server Error' });
+    }
+  });
+  
 
     //insert cars data to db :use post metod
 
